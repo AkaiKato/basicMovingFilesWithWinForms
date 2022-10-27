02a525974c3b05b4cfc289794b5422274a976bf4
AkaiKato <rail201@yandex.ru>
2022-10-27 19:20:02 +0300

    Добавьте файлы проекта.

---
+++ lab3HM/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace lab3HM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fromListBox = new System.Windows.Forms.ListBox();
+            this.toListBox = new System.Windows.Forms.ListBox();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.backFromButton = new System.Windows.Forms.Button();
+            this.backToButton = new System.Windows.Forms.Button();
+            this.fromTextBox = new System.Windows.Forms.TextBox();
+            this.toTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // fromListBox
+            // 
+            this.fromListBox.FormattingEnabled = true;
+            this.fromListBox.Location = new System.Drawing.Point(12, 46);
+            this.fromListBox.Name = "fromListBox";
+            this.fromListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.fromListBox.Size = new System.Drawing.Size(1006, 225);
+            this.fromListBox.TabIndex = 0;
+            this.fromListBox.DoubleClick += new System.EventHandler(this.fromListBox_DoubleClick);
+            // 
+            // toListBox
+            // 
+            this.toListBox.FormattingEnabled = true;
+            this.toListBox.Location = new System.Drawing.Point(12, 355);
+            this.toListBox.Name = "toListBox";
+            this.toListBox.Size = new System.Drawing.Size(1006, 225);
+            this.toListBox.TabIndex = 1;
+            this.toListBox.DoubleClick += new System.EventHandler(this.toListBox_DoubleClick);
+            // 
+            // moveButton
+            // 
+            this.moveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.moveButton.Location = new System.Drawing.Point(12, 277);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(1006, 38);
+            this.moveButton.TabIndex = 2;
+            this.moveButton.Text = "↓";
+            this.moveButton.UseVisualStyleBackColor = true;
+            this.moveButton.Click += new System.EventHandler(this.moveButton_Click);
+            // 
+            // backFromButton
+            // 
+            this.backFromButton.Image = ((System.Drawing.Image)(resources.GetObject("backFromButton.Image")));
+            this.backFromButton.Location = new System.Drawing.Point(12, 12);
+            this.backFromButton.Name = "backFromButton";
+            this.backFromButton.Size = new System.Drawing.Size(29, 28);
+            this.backFromButton.TabIndex = 3;
+            this.backFromButton.UseVisualStyleBackColor = true;
+            this.backFromButton.Click += new System.EventHandler(this.backFromButton_Click);
+            // 
+            // backToButton
+            // 
+            this.backToButton.Image = ((System.Drawing.Image)(resources.GetObject("backToButton.Image")));
+            this.backToButton.Location = new System.Drawing.Point(12, 321);
+            this.backToButton.Name = "backToButton";
+            this.backToButton.Size = new System.Drawing.Size(29, 28);
+            this.backToButton.TabIndex = 4;
+            this.backToButton.UseVisualStyleBackColor = true;
+            this.backToButton.Click += new System.EventHandler(this.backToButton_Click);
+            // 
+            // fromTextBox
+            // 
+            this.fromTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.fromTextBox.Location = new System.Drawing.Point(47, 13);
+            this.fromTextBox.Name = "fromTextBox";
+            this.fromTextBox.Size = new System.Drawing.Size(971, 27);
+            this.fromTextBox.TabIndex = 5;
+            this.fromTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.fromTextBox_KeyPress);
+            // 
+            // toTextBox
+            // 
+            this.toTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.toTextBox.Location = new System.Drawing.Point(47, 322);
+            this.toTextBox.Name = "toTextBox";
+            this.toTextBox.Size = new System.Drawing.Size(971, 27);
+            this.toTextBox.TabIndex = 6;
+            this.toTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.toTextBox_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 595);
+            this.Controls.Add(this.toTextBox);
+            this.Controls.Add(this.fromTextBox);
+            this.Controls.Add(this.backToButton);
+            this.Controls.Add(this.backFromButton);
+            this.Controls.Add(this.moveButton);
+            this.Controls.Add(this.toListBox);
+            this.Controls.Add(this.fromListBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "lab3hm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox fromListBox;
+        private System.Windows.Forms.ListBox toListBox;
+        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.Button backFromButton;
+        private System.Windows.Forms.Button backToButton;
+        private System.Windows.Forms.TextBox fromTextBox;
+        private System.Windows.Forms.TextBox toTextBox;
+    }
+}
+
---
+++ lab3HM/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3HM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            startFunc();
+        }
+
+        private static string fromDirName = Directory.GetCurrentDirectory();
+        private static string toDirName = Directory.GetCurrentDirectory();
+
+        static Regex regex = new Regex(@"([A-Za-z]:\\)((?:.*\\)?)");
+
+        public void startFunc()
+        {
+            fromTextBox.Text = fromDirName;
+            toTextBox.Text = toDirName;
+            fillListBox(fromListBox, fromTextBox);
+            fillListBox(toListBox, toTextBox);
+        }
+
+        private void backFromButton_Click(object sender, EventArgs e)
+        {
+            if (checkParentPath(fromDirName))
+            {
+                fromDirName = Directory.GetParent(fromDirName).ToString();
+                fromTextBox.Text = fromDirName;
+                fillListBox(fromListBox, fromTextBox);
+            }
+        }
+
+        private void backToButton_Click(object sender, EventArgs e)
+        {
+            if (checkParentPath(toDirName))
+            {
+                toDirName = Directory.GetParent(toDirName).ToString();
+                toTextBox.Text = toDirName;
+                fillListBox(toListBox, toTextBox);
+            }
+        }
+
+        private bool checkParentPath (string dirName)
+        {
+            if (Directory.GetParent(dirName) != null)
+                return true;
+            else
+            {
+                MessageBox.Show("Дальше вернуться невозможно!", "Внимание!");
+                return false;
+            }
+        }
+
+        private void fillListBox(ListBox listBox, TextBox path)
+        {
+            listBox.Items.Clear();
+
+            string[] dirs = Directory.GetDirectories(path.Text.ToString());
+            listBox.Items.AddRange(getNames(dirs));
+
+            string[] files = Directory.GetFiles(path.Text.ToString());
+            listBox.Items.AddRange(getNames(files));
+        }
+
+        private void checkRightPath(ListBox listBox, TextBox textBox, bool path)
+        {
+            if (regex.IsMatch(textBox.Text.ToString()) && Directory.Exists(textBox.Text.ToString()))
+            {
+                if (path)
+                    fromDirName = textBox.Text.ToString();
+                else
+                    toDirName = textBox.Text.ToString(); 
+                fillListBox(listBox, textBox);
+            }
+            else
+            {
+                MessageBox.Show("Проверьте правильность написания пути!", "Внимание!");
+                if (path)
+                    textBox.Text = fromDirName;
+                else
+                    textBox.Text = toDirName;
+            }
+        }
+
+        private void fromTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+                checkRightPath(fromListBox, fromTextBox, true);
+        }
+
+        private void toTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+                checkRightPath(toListBox, toTextBox, false);
+        }
+
+        private string[] getNames(string[] strings)
+        {
+            string[] dirsName = new string[strings.Length];
+            for (int i = 0; i < strings.Length; i++)
+            {
+                var buf = strings[i].ToString().Split('\\');
+                dirsName[i] = buf[buf.Length - 1];
+            }
+            return dirsName;
+        }
+
+        private void moveButton_Click(object sender, EventArgs e)
+        {
+            var selectedItems = fromListBox.SelectedItems;
+
+            foreach (var item in selectedItems)
+            {
+                string filePath = fromDirName + "\\" + item.ToString();
+                string destPath = toDirName + "\\" + item.ToString();
+                if(File.Exists(filePath))
+                    File.Move(filePath, destPath);
+                if (Directory.Exists(filePath))
+                {
+                    if (filePath != toDirName && filePath != destPath)
+                        Directory.Move(filePath, destPath);
+                    else if (filePath == destPath)
+                        MessageBox.Show($"Исходный и результирующий путь должны различаться.", "Внимание!");
+                    else
+                        MessageBox.Show($"Невозможно переместить папку: {item.ToString()} \nТак как в нее происходит перемещение.", "Внимание!");
+                }
+            }
+            fillListBox(fromListBox, fromTextBox);
+            fillListBox(toListBox, toTextBox);
+        }
+
+        private void fromListBox_DoubleClick(object sender, EventArgs e)
+        {
+            if(Directory.Exists(fromDirName + "\\" + fromListBox.SelectedItem))
+            {
+                fromDirName = fromDirName + "\\" + fromListBox.SelectedItem;
+                fromTextBox.Text = fromDirName;
+                fillListBox(fromListBox, fromTextBox);
+            }
+        }
+
+        private void toListBox_DoubleClick(object sender, EventArgs e)
+        {
+            if (Directory.Exists(toDirName + "\\" + toListBox.SelectedItem))
+            {
+                toDirName = toDirName + "\\" + toListBox.SelectedItem;
+                toTextBox.Text = toDirName;
+                fillListBox(toListBox, toTextBox);
+            }
+        }
+    }
+}
